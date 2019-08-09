32b4d6f613c0363fc32f83208f09b4a6174e9273
Samvel <aphakob@mail.ru>
2019-08-09 19:41:09 +0400

    dasssss3
    Signed-off-by: Samvel <aphakob@mail.ru>

---
+++ app/src/main/java/com/example/das3/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.das3;
+
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] country = { "India", "USA", "China", "Japan", "Other"};
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+    Button btn,btn2;
+    AlertDialog.Builder alert;
+    RatingBar rating;
+    SeekBar seek;
+    DatePicker peeker1;
+    TimePicker peeker2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        getAlert();
+//        getRating();
+//        getSeek();
+//        getDate();
+//        getTime();
+
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
+        spin.setOnItemSelectedListener(this);
+        spin1.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin1.setAdapter(aa1);
+    }
+
+    private void getTime() {
+        peeker2 = (TimePicker)findViewById(R.id.peeker2);
+        peeker2.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                String str = timePicker.getHour()+"/"+timePicker.getMinute();
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void getDate() {
+        peeker1 = (DatePicker)findViewById(R.id.peeker1);
+//        peeker1.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+//            @Override
+//            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+//                String str = datePicker.getDayOfMonth() + "/" + datePicker.getDayOfMonth() + "/" + datePicker.getYear();
+//                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+
+    private void getSeek() {
+        seek =(SeekBar) findViewById(R.id.peeker);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Toast.makeText(getApplicationContext(),String.valueOf(i),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"a",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"b",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getRating() {
+        btn2   = (Button)findViewById(R.id.btn2);
+        rating = (RatingBar)findViewById(R.id.rating);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = String.valueOf(rating.getRating());
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
+            }
+        });
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                String str = String.valueOf(rating.getRating());
+                Toast.makeText(getApplicationContext(),String.valueOf(v),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getAlert() {
+        btn   = (Button)findViewById(R.id.button);
+        alert = new AlertDialog.Builder(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alert.setMessage("aaaa")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        Toast.makeText(getApplicationContext(),"yes",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        Toast.makeText(getApplicationContext(),"no",Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog alertDialog = alert.create();
+                alertDialog.setTitle("Hello");
+                alertDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getApplicationContext(),country[i] , Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
